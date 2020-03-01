22a5422b8665c597c287d36f97cab5ecbc3e0d5c
schadokar <shubhamchadokar04@gmail.com>
2020-03-01 10:59:43 +0530

    Updates

---
+++ precache-manifest.5904e975998c5e016f8cf52c0510da83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b681d967299307af161b3cbd5fddb8",
+    "url": "/my-task-list/index.html"
+  },
+  {
+    "revision": "e7a58083ec0604dde9fa",
+    "url": "/my-task-list/static/css/main.5ec7c972.chunk.css"
+  },
+  {
+    "revision": "c3a2e6a380d36c077b2a",
+    "url": "/my-task-list/static/js/2.930a2e97.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/my-task-list/static/js/2.930a2e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a58083ec0604dde9fa",
+    "url": "/my-task-list/static/js/main.826f21bd.chunk.js"
+  },
+  {
+    "revision": "de2c52fa9e2ac9aab4e2",
+    "url": "/my-task-list/static/js/runtime-main.dab31681.js"
+  }
+]);
